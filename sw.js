5d068122141356b99978a5018d330f5ff77e3dbd
Reekendra yadav <reekendray461@gmail.com>
2019-05-26 18:46:36 +0530

    adding dynamic caching

---
+++ sw.js
@@ -0,0 +1,56 @@
+//install service worker
+
+const staticCacheName = "site-static-v2";
+const dynamicCache = "site-dynamic-v1";
+const assets = [
+  "/",
+  "/index.html",
+  "/js/app.js",
+  "/js/ui.js",
+  "/js/materialize.min.js",
+  "/css/styles.css",
+  "/css/materialize.min.css",
+  "/img/dish.png",
+  "https://fonts.googleapis.com/icon?family=Material+Icons",
+  "https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2"
+];
+self.addEventListener("install", evt => {
+  //console.log("service worker has been installed");
+  evt.waitUntil(
+    caches.open(staticCacheName).then(cache => {
+      console.log("caching shell assets");
+      cache.addAll(assets);
+    })
+  );
+});
+
+//activate service worker
+self.addEventListener("activate", evt => {
+  //console.log("service worker has been activated");
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== staticCacheName)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+//fetch event
+self.addEventListener("fetch", evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return (
+        cacheRes ||
+        fetch(evt.request).then(fetchRes => {
+          return caches.open(dynamicCache).then(cache => {
+            cache.put(evt.request.url, fetchRes.clone());
+            return fetchRes;
+          });
+        })
+      );
+    })
+  );
+});
